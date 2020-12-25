e71337f8b7a479ee14179bd906a013925be01e73
GabrielTeo <gabrieltyx@gmail.com>
2020-12-25 15:09:34 +0800

    Add ball image

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,21 @@ class BallPage extends StatelessWidget {
         title: Text('Ask Me Anything'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
     );
   }
 }
